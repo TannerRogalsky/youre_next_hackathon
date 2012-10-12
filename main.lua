a70a77b9b4307366c721e054d7e682ff79f42c6f
Tanner Rogalsky <the.t.dawg@gmail.com>
2012-10-12 19:01:12 -0400

    initial commit

---
+++ main.lua
@@ -0,0 +1,22 @@
+love.filesystem.load('requirements.lua')()
+
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.mousepressed(x, y, button)
+end
+
+function love.mousereleased(x, y, button)
+end
+
+function love.keypressed(key, unicode)
+end
+
+function love.keyreleased(key, unicode)
+end
+
+function love.draw()
+end
---
+++ states/loading.lua
@@ -0,0 +1,12 @@
+local Loading = Game:addState('Loading')
+
+function Loading:enteredState()
+
+  print('Entered Loading')
+
+end
+
+function Loading:render()
+end
+
+return Loading
